c8f0bc670498bfeec1708e0ad1d0eb7b34d97e3b
Alisher1504 <ruzmetov150499@gmail.com>
2022-11-01 19:46:02 +0500

    finish

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,8 +7,8 @@
         <title>Laravel</title>
 
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
 
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -50,7 +50,7 @@
                             
                             <div class="row align-items-center">
 
-                            <header id="header" class="">
+                            <header id="header" class="fixed-top">
                                 <div class="container d-flex align-items-center">
 
                                 <h1 class="logo me-auto"><a href="index.html">Mentor</a></h1>
@@ -94,112 +94,32 @@
 
                                 <!-- ======= About Section ======= -->
                                 <section id="about" class="about">
-                                <div class="container" data-aos="fade-up">
+                                    <div class="container" data-aos="fade-up">
 
-                                    <div class="row">
-                                    <div class="col-lg-6 order-1 order-lg-2" data-aos="fade-left" data-aos-delay="100">
-                                        <img src="assets/img/about.jpg" class="img-fluid" alt="">
-                                    </div>
-                                    <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content">
-                                        <h3>Voluptatem dignissimos provident quasi corporis voluptates sit assumenda.</h3>
-                                        <p class="fst-italic">
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-                                        magna aliqua.
-                                        </p>
-                                        <ul>
-                                        <li><i class="bi bi-check-circle"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
-                                        <li><i class="bi bi-check-circle"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
-                                        <li><i class="bi bi-check-circle"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</li>
-                                        </ul>
-                                        <p>
-                                        Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                                        </p>
+                                        <div class="row align-items-center">
+                                        <div class="col-lg-6 order-1 order-lg-2" data-aos="fade-left" data-aos-delay="100">
+                                            <img src="assets/img/about.jpg" class="img-fluid" alt="">
+                                        </div>
+                                        <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content">
+                                            <h3>Voluptatem dignissimos provident quasi corporis voluptates sit assumenda.</h3>
+                                            <p class="fst-italic">
+                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+                                            magna aliqua.
+                                            </p>
+                                            <ul>
+                                            <li><i class="bi bi-check-circle"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
+                                            <li><i class="bi bi-check-circle"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
+                                            <li><i class="bi bi-check-circle"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</li>
+                                            </ul>
+                                            <p>
+                                                Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                                            </p>
+
+                                        </div>
+                                        </div>
 
                                     </div>
-                                    </div>
-
-                                </div>
                                 </section><!-- End About Section -->
-
-                                <!-- ======= Counts Section ======= -->
-                                <section id="counts" class="counts section-bg">
-                                <div class="container">
-
-                                    <div class="row counters">
-
-                                    <div class="col-lg-3 col-6 text-center">
-                                        <span data-purecounter-start="0" data-purecounter-end="1232" data-purecounter-duration="1" class="purecounter"></span>
-                                        <p>Students</p>
-                                    </div>
-
-                                    <div class="col-lg-3 col-6 text-center">
-                                        <span data-purecounter-start="0" data-purecounter-end="64" data-purecounter-duration="1" class="purecounter"></span>
-                                        <p>Courses</p>
-                                    </div>
-
-                                    <div class="col-lg-3 col-6 text-center">
-                                        <span data-purecounter-start="0" data-purecounter-end="42" data-purecounter-duration="1" class="purecounter"></span>
-                                        <p>Events</p>
-                                    </div>
-
-                                    <div class="col-lg-3 col-6 text-center">
-                                        <span data-purecounter-start="0" data-purecounter-end="15" data-purecounter-duration="1" class="purecounter"></span>
-                                        <p>Trainers</p>
-                                    </div>
-
-                                    </div>
-
-                                </div>
-                                </section><!-- End Counts Section -->
-
-                                <!-- ======= Why Us Section ======= -->
-                                <section id="why-us" class="why-us">
-                                <div class="container" data-aos="fade-up">
-
-                                    <div class="row">
-                                    <div class="col-lg-4 d-flex align-items-stretch">
-                                        <div class="content">
-                                        <h3>Why Choose Mentor?</h3>
-                                        <p>
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Duis aute irure dolor in reprehenderit
-                                            Asperiores dolores sed et. Tenetur quia eos. Autem tempore quibusdam vel necessitatibus optio ad corporis.
-                                        </p>
-                                        <div class="text-center">
-                                            <a href="about.html" class="more-btn">Learn More <i class="bx bx-chevron-right"></i></a>
-                                        </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-8 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
-                                        <div class="icon-boxes d-flex flex-column justify-content-center">
-                                        <div class="row">
-                                            <div class="col-xl-4 d-flex align-items-stretch">
-                                            <div class="icon-box mt-4 mt-xl-0">
-                                                <i class="bx bx-receipt"></i>
-                                                <h4>Corporis voluptates sit</h4>
-                                                <p>Consequuntur sunt aut quasi enim aliquam quae harum pariatur laboris nisi ut aliquip</p>
-                                            </div>
-                                            </div>
-                                            <div class="col-xl-4 d-flex align-items-stretch">
-                                            <div class="icon-box mt-4 mt-xl-0">
-                                                <i class="bx bx-cube-alt"></i>
-                                                <h4>Ullamco laboris ladore pan</h4>
-                                                <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt</p>
-                                            </div>
-                                            </div>
-                                            <div class="col-xl-4 d-flex align-items-stretch">
-                                            <div class="icon-box mt-4 mt-xl-0">
-                                                <i class="bx bx-images"></i>
-                                                <h4>Labore consequatur</h4>
-                                                <p>Aut suscipit aut cum nemo deleniti aut omnis. Doloribus ut maiores omnis facere</p>
-                                            </div>
-                                            </div>
-                                        </div>
-                                        </div><!-- End .content-->
-                                    </div>
-                                    </div>
-
-                                </div>
-                                </section><!-- End Why Us Section -->
 
                                 <!-- ======= Features Section ======= -->
                                 <section id="features" class="features">
@@ -208,73 +128,74 @@
                                     <div class="row" data-aos="zoom-in" data-aos-delay="100">
                                     <div class="col-lg-3 col-md-4">
                                         <div class="icon-box">
-                                        <i class="ri-store-line" style="color: #ffbb2c;"></i>
+                                        <!-- <i class="ri-store-line" style="color: #ffbb2c;"></i> -->
+                                        <i class="bi bi-alarm" style="color: #ffbb2c;"></i>
                                         <h3><a href="">Lorem Ipsum</a></h3>
                                         </div>
                                     </div>
                                     <div class="col-lg-3 col-md-4 mt-4 mt-md-0">
                                         <div class="icon-box">
-                                        <i class="ri-bar-chart-box-line" style="color: #5578ff;"></i>
+                                        <i class="bi bi-bag" style="color: #5578ff;"></i>
                                         <h3><a href="">Dolor Sitema</a></h3>
                                         </div>
                                     </div>
                                     <div class="col-lg-3 col-md-4 mt-4 mt-md-0">
                                         <div class="icon-box">
-                                        <i class="ri-calendar-todo-line" style="color: #e80368;"></i>
+                                        <i class="bi bi-basket2-fill" style="color: #e80368;"></i>
                                         <h3><a href="">Sed perspiciatis</a></h3>
                                         </div>
                                     </div>
                                     <div class="col-lg-3 col-md-4 mt-4 mt-lg-0">
                                         <div class="icon-box">
-                                        <i class="ri-paint-brush-line" style="color: #e361ff;"></i>
+                                        <i class="bi bi-battery-half" style="color: #e361ff;"></i>
                                         <h3><a href="">Magni Dolores</a></h3>
                                         </div>
                                     </div>
                                     <div class="col-lg-3 col-md-4 mt-4">
                                         <div class="icon-box">
-                                        <i class="ri-database-2-line" style="color: #47aeff;"></i>
+                                        <i class="bi bi-box-seam" style="color: #47aeff;"></i>
                                         <h3><a href="">Nemo Enim</a></h3>
                                         </div>
                                     </div>
                                     <div class="col-lg-3 col-md-4 mt-4">
                                         <div class="icon-box">
-                                        <i class="ri-gradienter-line" style="color: #ffa76e;"></i>
+                                        <i class="bi bi-browser-firefox" style="color: #ffa76e;"></i>
                                         <h3><a href="">Eiusmod Tempor</a></h3>
                                         </div>
                                     </div>
                                     <div class="col-lg-3 col-md-4 mt-4">
                                         <div class="icon-box">
-                                        <i class="ri-file-list-3-line" style="color: #11dbcf;"></i>
+                                        <i class="bi bi-browser-chrome" style="color: #11dbcf;"></i>
                                         <h3><a href="">Midela Teren</a></h3>
                                         </div>
                                     </div>
                                     <div class="col-lg-3 col-md-4 mt-4">
                                         <div class="icon-box">
-                                        <i class="ri-price-tag-2-line" style="color: #4233ff;"></i>
+                                        <i class="bi bi-calendar-month" style="color: #4233ff;"></i>
                                         <h3><a href="">Pira Neve</a></h3>
                                         </div>
                                     </div>
                                     <div class="col-lg-3 col-md-4 mt-4">
                                         <div class="icon-box">
-                                        <i class="ri-anchor-line" style="color: #b2904f;"></i>
+                                        <i class="bi bi-calendar2-plus" style="color: #b2904f;"></i>
                                         <h3><a href="">Dirada Pack</a></h3>
                                         </div>
                                     </div>
                                     <div class="col-lg-3 col-md-4 mt-4">
                                         <div class="icon-box">
-                                        <i class="ri-disc-line" style="color: #b20969;"></i>
+                                        <i class="bi bi-capslock-fill" style="color: #b20969;"></i>
                                         <h3><a href="">Moton Ideal</a></h3>
                                         </div>
                                     </div>
                                     <div class="col-lg-3 col-md-4 mt-4">
                                         <div class="icon-box">
-                                        <i class="ri-base-station-line" style="color: #ff5828;"></i>
+                                        <i class="bi bi-cart-plus-fill" style="color: #ff5828;"></i>
                                         <h3><a href="">Verdo Park</a></h3>
                                         </div>
                                     </div>
                                     <div class="col-lg-3 col-md-4 mt-4">
                                         <div class="icon-box">
-                                        <i class="ri-fingerprint-line" style="color: #29cc61;"></i>
+                                        <i class="bi bi-clipboard-pulse" style="color: #29cc61;"></i>
                                         <h3><a href="">Flavor Nivelanda</a></h3>
                                         </div>
                                     </div>
@@ -513,11 +434,11 @@
                                     </div>
                                 </div>
                                 <div class="social-links text-center text-md-right pt-3 pt-md-0">
-                                    <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-                                    <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-                                    <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-                                    <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-                                    <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+                                    <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+                                    <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+                                    <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+                                    <a href="#" class="google-plus"><i class="bi bi-skype"></i></a>
+                                    <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
                                 </div>
                                 </div>
                             </footer><!-- End Footer -->
